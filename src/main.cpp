7811f648efc32804552cabc103df2b5cc507cd30
Aleeshba <alishbahshareef@gmail.com>
2024-02-03 22:59:48 +0500

    Stress Relieving Application

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "menu.hpp"
-using namespace std;
-int main(int argc, char* argv[]){
-    //creating an object of main menu class where the for Main manu is encapsulated
-    MainMenu obj;
-    obj.run();
-    return 0;
-}
